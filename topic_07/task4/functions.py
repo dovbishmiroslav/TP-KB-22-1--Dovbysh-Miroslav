1c5a58962522a9e01a4d9a94e77d2886ab345134
dovbishmiroslav <dovbish.miroslav@gmail.com>
2025-12-07 12:27:28 +0200

    Add files via upload

---
+++ topic_07/task4/functions.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Ділення на 0 неможливе")
+        return None
